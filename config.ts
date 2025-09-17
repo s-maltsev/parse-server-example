11fe3141a1f09e5b5e495ba116fe6cdbbbe00eed
s-maltsev <105911370+s-maltsev@users.noreply.github.com>
2025-09-17 19:03:47 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,11 +2,11 @@ import { schemaDefinitions } from './cloud/schema.js';
 
 export const config = {
   databaseURI:
-    process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
+    process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb+srv://ALEBITracker:94Bk.FHpX_@erjikcluster.xojylrb.mongodb.net/?retryWrites=true&w=majority&appName=ErjikCluster',
   cloud: () => import('./cloud/main.js'),
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'biathlonApp',
+  masterKey: process.env.MASTER_KEY || 'Alebi2025', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://parse-server-example-f148.onrender.com/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
