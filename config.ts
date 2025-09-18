3f1a55baf5b784756d872ac974773eb0b1efdaeb
s-maltsev <105911370+s-maltsev@users.noreply.github.com>
2025-09-18 11:11:05 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,22 @@
 import { schemaDefinitions } from './cloud/schema.js';
 
 export const config = {
-  databaseURI:
+  /*databaseURI:
     process.env.DATABASE_URI || process.env.MONGODB_URI,
   cloud: () => import('./cloud/main.js'),
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
   allowOrigin: process.env.FRONTEND_URL || '*',
+  liveQuery: {
+    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+  },*/
+  databaseURI:
+    process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
+  cloud: () => import('./cloud/main.js'),
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
