f919eb68afaca579ff3c3c785bc60d6d9c75a0d9
s-maltsev <105911370+s-maltsev@users.noreply.github.com>
2025-09-18 10:29:41 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,12 +2,12 @@ import { schemaDefinitions } from './cloud/schema.js';
 
 export const config = {
   databaseURI:
-    process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb+srv://ALEBITracker:94Bk.FHpX_@erjikcluster.xojylrb.mongodb.net/?retryWrites=true&w=majority&appName=ErjikCluster',
+    process.env.DATABASE_URI || process.env.MONGODB_URI,
   cloud: () => import('./cloud/main.js'),
-  appId: process.env.APP_ID || 'biathlonApp',
-  masterKey: process.env.MASTER_KEY || 'Alebi2025', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://parse-server-example-f148.onrender.com/parse', // Don't forget to change to https if needed
-  allowOrigin: "*",
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
+  allowOrigin: process.env.FRONTEND_URL,
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
